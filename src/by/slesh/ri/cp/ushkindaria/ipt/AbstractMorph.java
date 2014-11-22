f9ffd1b435b69a9bea84d97f2cb343b2271ec2b1
sleshDev <slesh.eugene93@gmail.com>
2014-11-22 19:08:48 +0200

    fixed morph operators : erode and dilate

--- src/by/slesh/ri/cp/ushkindaria/ipt/AbstractMorph.java
+++ src/by/slesh/ri/cp/ushkindaria/ipt/AbstractMorph.java
@@ -1,7 +1,6 @@
 package by.slesh.ri.cp.ushkindaria.ipt;
 
 import java.awt.image.BufferedImage;
-import java.util.Comparator;
 
 public abstract class AbstractMorph extends AbstractTool {
 	
@@ -30,67 +29,29 @@ public abstract class AbstractMorph extends AbstractTool {
     };
 	
 	public static final int[][]		CIRCLE_KERNEL	= new int[][] {
-		{_0, _1, _0,},
-		{_1, _1, _1,},
-		{_0, _1, _0,},
+		{_1, _1,},
+		{_1, _1,},
     };
+//		{_0, _1, _0,},
 //		{_1, _1, _1,},
-//		{_0, _0, _0,},
-//		{_1, _1, _1,},
+//		{_0, _1, _0,},
+//    };
+//		{_0, _1, _1, _0},
+//		{_1, _1, _1, _1},
+//		{_1, _1, _1, _1},
+//		{_0, _1, _1, _0},
 //    };
 //	{_1, _0, _1,},
 //	{_1, _0, _1,},
 //	{_1, _0, _1,},
 //};
-//		{_1, _0, _1,},
+//		{_1, _1, _1,},
 //		{_0, _0, _0,},
-//		{_1, _0, _1,},
+//		{_1, _1, _1,},
 //	};
 
-	protected Comparator<Integer>	checkExecute;
-	protected Comparable<Integer>	checkPixel;
 	protected int[][]				kernel;
-	protected boolean				initExecuteState;
-	protected int					newPixelValue;
 	
 	
-	public BufferedImage morph(BufferedImage source) {
-
-		int heightKernel = kernel.length;
-		int widthKernel = kernel[0].length;
-		int w = source.getWidth();
-		int h = source.getHeight();
-		int[] sourceRgb = source.getRGB(0, 0, w, h, null, w, w);
-		int[] targetRgb = sourceRgb.clone();
-		for (int y = 0; y < h; ++y) {
-			for (int x = 0; x < w; ++x) {
-				int currentPixel = sourceRgb[y * w + x];
-				if (checkPixel.compareTo(currentPixel) == 0) // 0 equal true
-					continue;
-				boolean isScan = true;
-				boolean isExecute = initExecuteState;
-				for (int i = 0; (i < heightKernel) && isScan; ++i) {
-					for (int j = 0; (j < widthKernel) && isScan; ++j) {
-						int pixelPosY = (y - heightKernel / 2) + i;
-						int pixelPosX = (x - widthKernel / 2) + j;
-						if ((pixelPosY < 0 || pixelPosY >= h) || 
-							(pixelPosX < 0 || pixelPosX >= w) || 
-							(i == y && j == x)) {
-							continue;
-						}
-						int maskPixel = kernel[i][j];
-						int underPixel = sourceRgb[w * pixelPosY + pixelPosX];
-						if (checkExecute.compare(underPixel, maskPixel) == 0) {
-							isExecute = !isExecute;
-							isScan = false;
-						}
-					}
-				}
-				if (isExecute) {
-					targetRgb[y * w + x] = newPixelValue;
-				}
-			}
-		}
-		return rgbToImage(targetRgb, w, h);
-	}
+	public abstract BufferedImage morph(BufferedImage source);
 }
--- src/by/slesh/ri/cp/ushkindaria/ipt/ErodeMorph.java
+++ src/by/slesh/ri/cp/ushkindaria/ipt/ErodeMorph.java
@@ -1,29 +1,50 @@
 package by.slesh.ri.cp.ushkindaria.ipt;
 
-import java.util.Comparator;
+import java.awt.image.BufferedImage;
 
 public class ErodeMorph extends AbstractMorph {
 
 	public ErodeMorph() {
 
 		kernel = CIRCLE_KERNEL;
-		checkExecute = new Comparator<Integer>() {
+	}
+	
+	@Override
+	public BufferedImage morph(BufferedImage source) {
 
-			@Override
-			public int compare(Integer imagePixel, Integer maskPixel) {
+		int heightKernel = kernel.length;
+		int widthKernel = kernel[0].length;
+		int w = source.getWidth();
+		int h = source.getHeight();
+		int[] sourceRgb = source.getRGB(0, 0, w, h, null, w, w);
+		int[] targetRgb = sourceRgb.clone();
+		for (int y = 0; y < h; ++y) {
+			for (int x = 0; x < w; ++x) {
 
-				return (imagePixel - _0) + (maskPixel - _1);
+				int index = y * w + x;
+				if (sourceRgb[index] == AbstractTool._0) {
+					continue;
+				}
+
+				for (int i = 0; i < heightKernel; ++i) {
+
+					for (int j = 0; j < widthKernel; ++j) {
+
+						int pixelPosY = (y - heightKernel / 2) + i;
+						int pixelPosX = (x - widthKernel / 2) + j;
+						if ((pixelPosY < 0 || pixelPosY >= h) || (pixelPosX < 0 || pixelPosX >= w)
+								|| (i == y && j == x)) {
+							continue;
+						}
+						if (kernel[i][j] == AbstractTool._1 && sourceRgb[w * pixelPosY + pixelPosX] == AbstractTool._0) {
+							targetRgb[index] = AbstractTool._0;
+							i = heightKernel;
+							j = widthKernel;
+						}
+					}
+				}
 			}
-		};
-		checkPixel = new Comparable<Integer>() {
-
-			@Override
-			public int compareTo(Integer imagePixel) {
-
-				return imagePixel - _0;
-			}
-		};
-		newPixelValue = _0;
-		initExecuteState = false;
+		}
+		return rgbToImage(targetRgb, w, h);
 	}
 }
